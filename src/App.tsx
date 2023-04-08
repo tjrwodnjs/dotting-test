77b1258a7028c3c99dc96ded5a3eeeb4fddd9ba2
Kim Dong Hun <hunkim98@gmail.com>
2023-04-08 15:54:49 +0900

    Create scripts for running dotting library locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrushMode, Dotting, DottingRef, useBrush, useDotting } from "dotting";
 
 function App() {
+  const ref = useRef<DottingRef>(null);
+  const { changeBrushMode } = useBrush(ref);
+  const { undo } = useDotting(ref);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      if (e.code === "KeyZ" && (e.ctrlKey || e.metaKey)) {
+        console.log("hi");
+        undo();
+      }
+    });
+  }, [undo]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Dotting width={300} height={300} ref={ref} />
+        {/* <Artboard tool={brush} style={{ width: 800, height: 600 }} /> */}
+        <p
+          onClick={() => {
+            changeBrushMode(BrushMode.ERASER);
+          }}
+        >
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
