6469486e4ed103e93a637aae2fdc408ad4fc473c
Hun Kim <hunkim98@gmail.com>
2023-09-02 11:48:03 +0900

    Update code for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,103 @@
-import { useRef } from "react";
-import "./App.css";
-import { BrushMode, Dotting, DottingRef, useBrush } from "dotting";
+import { useEffect, useRef, useState } from "react";
+import {
+  CanvasHoverPixelChangeHandler,
+  Dotting,
+  DottingRef,
+  useBrush,
+  useData,
+  useDotting,
+  useGrids,
+  useHandlers,
+} from "dotting";
 
 function App() {
   const ref = useRef<DottingRef>(null);
-  const { changeBrushMode } = useBrush(ref);
-  // const { undo } = useDotting(ref);
+  const { colorPixels } = useDotting(ref);
+  const { indices, dimensions } = useGrids(ref);
+  const {
+    addHoverPixelChangeListener,
+    removeHoverPixelChangeListener,
+    addCanvasElementEventListener,
+    removeCanvasElementEventListener,
+  } = useHandlers(ref);
+  const [hoveredPixel, setHoveredPixel] = useState<{
+    rowIndex: number;
+    columnIndex: number;
+  } | null>(null);
 
-  // useEffect(() => {
-  //   document.addEventListener("keydown", (e) => {
-  //     if (e.code === "KeyZ" && (e.ctrlKey || e.metaKey)) {
-  //       console.log("hi");
-  //       undo();
-  //     }
-  //   });
-  // }, [undo]);
+  useEffect(() => {
+    const hoverPixelChangeListener: CanvasHoverPixelChangeHandler = (pixel) => {
+      const { indices } = pixel;
+      if (indices) {
+        setHoveredPixel(indices);
+      } else {
+        setHoveredPixel(null);
+      }
+    };
+    addHoverPixelChangeListener(hoverPixelChangeListener);
+    return () => {
+      removeHoverPixelChangeListener(hoverPixelChangeListener);
+    };
+  }, [addHoverPixelChangeListener, removeHoverPixelChangeListener]);
+
+  useEffect(() => {
+    const onCanvasClickListener = () => {
+      // TASK: Make a firework effect when the user clicks on the canvas.
+      // HINT1: You can use the `colorPixels` function to change the color of a pixel.
+      // HINT2: You must know the boundaries of the current pixel canvas to take into considuration of the extent of the firework effect.
+      // HINT3: You can use the indices and dimensions variables to get the boundaries of the current pixel canvas.
+      // Check out the documentation for more information:
+      // URL1: https://hunkim98.github.io/dotting/?path=/story/hooks-usedotting--page
+      // URL2: http://localhost:6005/?path=/story/hooks-usegrids--page
+      // Do not modify any parts other than the below.
+      // Modifiy ⬇️
+      if (hoveredPixel) {
+        console.log(
+          `You clicked on rowIndex: ${hoveredPixel.rowIndex}, columnIndex: ${hoveredPixel.columnIndex}`
+        );
+      }
+      // Modify ⬆️
+    };
+    addCanvasElementEventListener("mousedown", onCanvasClickListener);
+    return () => {
+      removeCanvasElementEventListener("mousedown", onCanvasClickListener);
+    };
+  }, [
+    addCanvasElementEventListener,
+    removeCanvasElementEventListener,
+    hoveredPixel,
+    indices,
+    colorPixels,
+    dimensions,
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Dotting width={300} height={300} ref={ref} />
-        {/* <Artboard tool={brush} style={{ width: 800, height: 600 }} /> */}
-        <p
-          onClick={() => {
-            changeBrushMode(BrushMode.ERASER);
+    <div
+      style={{
+        backgroundColor: "#282c34",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        color: "white",
+        position: "relative",
+      }}
+    >
+      {hoveredPixel && (
+        <div
+          style={{
+            position: "absolute",
+            transform: "translate(50%, 50%)",
+            right: "50%",
+            top: "10px",
           }}
         >
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          You are hoveing rowIndex: {hoveredPixel.rowIndex}, columnIndex:{" "}
+          {hoveredPixel.columnIndex}
+        </div>
+      )}
+      <Dotting width={500} height={500} ref={ref} />
     </div>
   );
 }
